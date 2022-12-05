13388b7f1cd769133781e4d681068c3a50a57300
Shahzad Ahmed <56846980+Shaze-ahmed@users.noreply.github.com>
2022-12-06 01:19:25 +0500

    Add files via upload

---
+++ 08 - dJango/Web Development/Class2/Saylani/polls/admin.py
@@ -0,0 +1,10 @@
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Question, Student, Choice
+
+admin.site.register(Question)
+admin.site.register(Student)
+admin.site.register(Choice)
+
